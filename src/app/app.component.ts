464e93e14fad2e645dfc9a1b2d0528555e9a13ca
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-10-04 16:08:20 -0300

    Consume the changes of the input box as an observable of string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WikipediaSearchService } from './wikipedia-search.service';
+import { Subject } from 'rxjs/Subject';
 
 // application wide shared Rx operators
 import 'rxjs/add/operator/map';
@@ -12,7 +13,10 @@ import 'rxjs/add/operator/map';
 
 export class AppComponent {
   items: Array<string>;
-  constructor(private service: WikipediaSearchService) { }
+  term$ = new Subject<string>();
+  constructor(private service: WikipediaSearchService) {
+    this.term$.subscribe(term => this.search(term));
+  }
 
   search(term: string) {
     this.service.search(term)
